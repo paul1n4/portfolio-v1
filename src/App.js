9d4a5d7f47c370c6ae917cf8089c745fb2263bd3
Pauline Val <pauline@Paulines-MacBook-Pro.local>
2022-05-12 16:00:00 -0500

    change homte to app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home'
+import {BrowserRouter as Router} from 'react-router-dom';
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Projects from './components/Projects'
+import Contact from "./components/Contact"
 
 function App() {
   return (
     <>
     <Router>
       <Navbar />
-      <Routes>
-        <Route exact path='/' element={<Home/>} />
-      </Routes>
+        <main className="home">
+          <Hero />
+          <About />
+          <Projects />
+          <Contact />
+        </main>
     </Router>
     </>
   );
